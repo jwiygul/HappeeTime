04b119c1d87e78e929b087d239752e701cc1fc9e
Jeremy Wiygul <wiggles1@jeremy-wiyguls-computer.local>
2012-10-29 11:34:08 -0400

    Initial Commit

---
+++ UroApp/UroAppAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UroAppAppDelegate.h
+//  UroApp
+//
+//  Created by Jeremy Wiygul on 10/29/12.
+//  Copyright (c) 2012 Jeremy Wiygul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UroAppAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UroApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UroApp
+//
+//  Created by Jeremy Wiygul on 10/29/12.
+//  Copyright (c) 2012 Jeremy Wiygul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UroAppAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UroAppAppDelegate class]));
+    }
+}
